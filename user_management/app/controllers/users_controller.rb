5334bfa23b4d752e39bbeec0857f197b03460203
Ms.Priyanka Bar <priyankabalasubramanya@gmail.com>
2016-10-27 17:06:29 +0530

    ROR user mgmt application

---
+++ user_management/app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+	def new
+		@user=User.new
+	end
+	def create
+		@user=User.new(user_params)
+		@user.save
+		# binding.pry
+		redirect_to users_list_path
+	end
+	def index
+		@users=User.all
+	end
+	def edit
+		@user=User.find_by_id(params[:id])
+
+	end
+	def update
+		@user=User.find_by_id(params[:id])
+		@user.update_attributes(user_params)
+		redirect_to users_list_path
+	end
+	def show
+		@user=User.find_by_id(params[:id])
+	end
+	def destroy
+		@user=User.find_by_id(params[:id])
+		@user.destroy
+		redirect_to users_list_path
+	end
+	private
+	def user_params
+		params.require(:user).permit(:firstname,:lastname,:email,:password)
+	end
+end
